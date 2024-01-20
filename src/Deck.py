c46bbca12a8398005195ce2c937bbefec0072555
Cerosop <jmhsu920816@gmail.com>
2024-01-20 23:29:25 +0800

    deck

--- src/Deck.py
+++ src/Deck.py
@@ -1,11 +1,11 @@
 class Deck:
     def __init__(self):
-        self.deck = [] #越前面越上面
+        self.deck = [] #越前面越上面 每次隨機生成(都要不一樣) ex['7 heart', 'A diamond', ...]代表愛心7在最上面方塊A第二
         self.deck_left = 52
         pass
 
-    def draw(self):
+    def draw(self): # 抽最上面一張出去
         pass
     
-    def deck_left(self):
+    def deck_left(self): # 排堆剩幾張
         pass
